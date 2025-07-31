e261e57ab96ada145fa10a932fc602b41642f929
Hoàng <124790953+Hoang-1910@users.noreply.github.com>
2025-07-31 17:11:53 +0700

    upload model

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'description', 'price', 'discount_price', 'stock_id', 'category_id', 'brand_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+    public function stock()
+    {
+        return $this->belongsTo(Stock::class);
+    }
+}
